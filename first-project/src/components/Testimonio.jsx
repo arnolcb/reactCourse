79666f9d9caa7024d1399deddbafc1ec7327a564
Arnol Cáceres <116393375+arnolcb@users.noreply.github.com>
2023-05-08 09:46:19 -0500

    finished project

--- first-project/src/components/Testimonio.jsx
+++ first-project/src/components/Testimonio.jsx
@@ -10,9 +10,15 @@ function Testimonio(props){
                 alt= 'Fotografía de Emma'
             />
             <div className='contenedor-texto-testimonio'>
-                <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
-                <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
-                <p className='texto-testimonio'>"{props.testimonio}"</p>
+                <p className='nombre-testimonio'>
+                    <strong>{props.nombre}</strong> en {props.pais}
+                </p>
+                <p className='cargo-testimonio'>
+                    {props.cargo} en <strong>{props.empresa}</strong>
+                </p>
+                <p className='texto-testimonio'>
+                    "{props.testimonio}"
+                </p>
             </div>
         </div>
     );
